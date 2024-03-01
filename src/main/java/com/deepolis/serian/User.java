9699833eda8f63153306422f2ddaedcaf1908bae
Arron Dick <arronmdick@gmail.com>
2024-03-01 16:48:04 -0600

    Repo

--- src/main/java/com/deepolis/serian/User.java
+++ src/main/java/com/deepolis/serian/User.java
@@ -1,9 +1,15 @@
 package com.deepolis.serian;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
+@Entity //specify as Table name
 public class User {
+    @Id //specify as primary key
     private String username;
     private String password;
+
+    public User() {}
 
     public String getUsername() {return username;}
     public void setUsername(String username) {this.username = username;}
